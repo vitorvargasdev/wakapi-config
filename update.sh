f278a0546396cec8e9b205c4cbc25a99cd0fd8a2
vitorvargasdev <instalando@protonmail.com>
2024-01-10 01:21:21 -0300

    refactor: should use amend instead new commit

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 cd ~/wakapi/wakadata
 wget http://localhost:4080/api/v1/users/vitorvargas/stats\?is_including_today\=true -O ~/wakapi/wakadata/wakapi.json
-git add . && git commit -m "Update wakapi.json" && git push origin main -f
-
+git add . && git commit --amend --no-edit && git push origin main -f
